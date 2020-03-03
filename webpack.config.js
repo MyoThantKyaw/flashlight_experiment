ca304b40a5317495ad12acfc283f73a1b2d1dab1
Myo Thant Kyaw <myothantkyaw21@gmail.com>
2020-03-03 23:41:10 +0630

    Added my project

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require("path")
+
+module.exports = {
+    mode: "development",
+    entry: {
+        app: "./src/app.js"
+    },
+    output: {
+        path : path.resolve("dis"),
+        filename: "app.bundle.3d.js"
+    },
+
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    }
+}
